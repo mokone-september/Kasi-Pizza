84e0dd4bfcaf05afe745c9c025606914cec7d9b3
mokone-september <mokoneseptember@gmail.com>
2025-06-03 20:36:57 +0200

    Remove unused className variable from Button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Button({ children, disabled, to, type, onClick }) {
-  const className =
-    "focus: inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 outline-none transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sn:px-6 sm:py-4";
 
   const base =
     "text-sm inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 outline-none transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
